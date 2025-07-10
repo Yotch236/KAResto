ab686ac4a73d9894f20c300284fbef85ee095536
Yotch236 <gerodayos23@gmail.com>
2025-07-10 15:04:05 +0800

    failed test

--- src/pages/Logout.js
+++ src/pages/Logout.js
@@ -1,18 +1,22 @@
-import { Navigate } from 'react-router-dom';
-import { useEffect, useContext, useState } from 'react';
-import { toast } from "react-toastify";
+import {Navigate} from 'react-router-dom';
+import {useEffect, useContext} from 'react';
+
 import UserContext from '../context/UserContext';
 
 export default function Logout() {
-  const { setUser, unsetUser } = useContext(UserContext);
-  const [loggedOut, setLoggedOut] = useState(false);
+	const { setUser, unsetUser } = useContext(UserContext)
 
-  useEffect(() => {
-    unsetUser();
-    setUser({ id: null, isAdmin: null });
-    toast.success("You have been logged out.");
-    setLoggedOut(true);
-  }, [unsetUser, setUser]);
 
-  return loggedOut ? <Navigate to="/login" replace /> : null;
-}
+	unsetUser()
+
+	useEffect(() => {
+		setUser({
+			id: null,
+			isAdmin: null
+		})
+	}, [setUser])
+
+	return (
+		<Navigate to='/login' />
+	)
+}   
